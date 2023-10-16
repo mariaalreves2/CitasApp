118b378e69c55cbd79dfb1a3f9d91c197af4bf89
mariaalreves2 <al269249@edu.uaa.mx>
2023-10-16 07:09:44 -0600

    Seccion06 - nuevos componentes

---
+++ SPA/src/app/_modules/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: false,
+      progressBar: true
+    }),
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
--- SPA/src/app/nav/nav.component.ts
+++ SPA/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { IUser } from '../_models/iuser';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,24 +12,22 @@ export class NavComponent implements OnInit{
   model: any = {};
   //currentUser$: Observable<IUser | null> =of (null);
 
-  constructor(public accountService: AccountService){}
+  constructor(public accountService: AccountService, 
+    private router: Router, 
+    private toastr: ToastrService){ }
 
   ngOnInit(): void {
-    //this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(): void{
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl("/members"),
+      error: error => this.toastr.error(error.error)
     })
-    
   }
 
   loggout(): void{
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
-
 }
