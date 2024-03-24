80fbfed6e2fc181cc979aa8a4cd2acb0c399cdb5
mariaalreves2 <airampedroza3@gmail.com>
2024-03-24 17:48:07 -0600

    Added the main photo thumbnail to nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,8 +46,9 @@ public class AccountController : BaseApiController
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(x =>
-        x.UserName.ToLower() == loginDto.Username.ToLower());
+        var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
 
         if(user == null) return Unauthorized(USER_PASSWORD_ERROR_MESSAGE);
 
@@ -63,7 +64,8 @@ public class AccountController : BaseApiController
 
         return new UserDto{
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
         };
 
     }
--- SPA/src/app/_services/account.service.ts
+++ SPA/src/app/_services/account.service.ts
@@ -21,8 +21,7 @@ export class AccountService {
       map((response: IUser) => {
         const user = response;
         if (user){
-          localStorage.setItem("user", JSON.stringify(user)); //Guardar en el localstorage el usuario
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -32,8 +31,7 @@ export class AccountService {
     return this.http.post<IUser>(this.baseUrl + "account/register", model).pipe(
       map(user =>{
         if(user){
-          localStorage.setItem("user",JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -42,6 +40,7 @@ export class AccountService {
 
   //Cualquier cambio que se necesite guardar 
   setCurrentUser(user: IUser){
+    localStorage.setItem("user",JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
