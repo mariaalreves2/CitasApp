87d117641b8087a6da446d86090f478cc1ae6c20
mariaalreves2 <airampedroza3@gmail.com>
2024-03-24 21:21:08 -0600

    Added new fields to register form

--- SPA/src/app/register/register.component.ts
+++ SPA/src/app/register/register.component.ts
@@ -22,10 +22,14 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = this.fb.group({
+      gender: ["female"],
       username: ["", Validators.required],
+      knownAs: ["", Validators.required],
+      dateOfBirth: ["", Validators.required],
+      city: ["", Validators.required],
+      country: ["", Validators.required],
       password: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
-      confirmPassword: ["", [Validators.required,
-      this.matchValues("password")]],
+      confirmPassword: ["", [Validators.required, this.matchValues("password")]],
     });
     
     this.registerForm.controls["password"].valueChanges.subscribe({
